c3939695470c050a79efff999f5ef941e7625a94
Greg Jones <ly@src.gnome.org>
2003-07-30 18:11:37 +0000

    Added system tray docklet Icons for sliders Shut down idle function when hidden

--- src/v4l.h
+++ src/v4l.h
@@ -15,6 +15,7 @@
 #include <png.h>
 #include <glade/glade.h>
 #include <gconf/gconf-client.h>
+#include "eggtrayicon.h"
 
 #define PICMAX 0
 #define PICMIN 1
@@ -24,45 +25,47 @@
 #define PPM 2
 
 typedef struct camera {
-   int dev;
-   int x;
-   int y;
-   int depth;
-   int desk_depth;
-   int size;
-   int dither;
-   int contrast, brightness, colour, hue, wb;
-   int frame_number;
-   struct video_capability vid_cap;
-   struct video_picture vid_pic;
-   struct video_window vid_win;
-   struct video_mbuf vid_buf;
-   struct video_mmap vid_map;
-   char *video_dev;
-   unsigned char *pic;
-   unsigned char *image;
-   char *capturefile, *rcapturefile;
-   gchar *pixdir, *rpixdir;
-   int savetype, rsavetype;
-   gchar *rhost, *rlogin, *rpw;
-   gchar *ts_string;
-   gchar *date_format;
-   gboolean debug, read;
-   gboolean cap, rcap, acap, show_adjustments;
-   gboolean timestamp, rtimestamp, usedate, usestring;
-   gboolean rtimefn, timefn;
-   GdkPixmap *pixmap;
-   GtkWidget *da, *tooltips, *status;
-   unsigned char *pic_buf, *tmp;
-   guint timeout_id;
-   guint32 timeout_interval;
-   GConfClient *gc;
-   GladeXML *xml;
+    int dev;
+    int x;
+    int y;
+    int depth;
+    int desk_depth;
+    int size;
+    int dither;
+    int contrast, brightness, colour, hue, wb;
+    int frame_number;
+    struct video_capability vid_cap;
+    struct video_picture vid_pic;
+    struct video_window vid_win;
+    struct video_mbuf vid_buf;
+    struct video_mmap vid_map;
+    char *video_dev;
+    unsigned char *pic;
+    unsigned char *image;
+    char *capturefile, *rcapturefile;
+    gchar *pixdir, *rpixdir;
+    int savetype, rsavetype;
+    gchar *rhost, *rlogin, *rpw;
+    gchar *ts_string;
+    gchar *date_format;
+    gboolean debug, read, hidden;
+    gboolean cap, rcap, acap, show_adjustments;
+    gboolean timestamp, rtimestamp, usedate, usestring;
+    gboolean rtimefn, timefn;
+    GdkPixmap *pixmap;
+	 GdkWindow *window;
+    GtkWidget *da, *tooltips, *tray_tooltip, *status;
+    unsigned char *pic_buf, *tmp;
+    guint timeout_id, idle_id;
+    guint32 timeout_interval;
+    GConfClient *gc;
+    GladeXML *xml;
+    EggTrayIcon *tray_icon;
 } cam;
 
-void camera_cap(cam *);
-void set_win_info(cam * cam);
-void get_pic_info(cam *);
-void set_pic_info(cam *);
-void get_win_info(cam *);
-void set_buffer(cam *);
+void camera_cap (cam *);
+void set_win_info (cam * cam);
+void get_pic_info (cam *);
+void set_pic_info (cam *);
+void get_win_info (cam *);
+void set_buffer (cam *);

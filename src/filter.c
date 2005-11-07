e7bee13fe06486ea137b9b8f539074235922d0cf
Sven Herzberg <herzi@gnome-de.org>
2005-11-07 18:21:23 +0000

    added information about current maintainership removed some unwanted
    2005-11-07  Sven Herzberg  <herzi@gnome-de.org>
    
            * AUTHORS: added information about current maintainership
    	* src/filter.c: removed some unwanted output
    	* src/v4l.c: removed some unwanted output

--- src/filter.c
+++ src/filter.c
@@ -11,34 +11,39 @@ static inline void move_420_block (int yTL, int yTR, int yBL, int yBR, int u,
 #define LIMIT(x) ((x)>0xffffff?0xff: ((x)<=0xffff?0:((x)>>16)))
 
 void
-yuv420p_to_rgb (unsigned char *image, unsigned char *temp, int x, int y,
-                int z){
-    const int numpix = x * y;
-    const int bytes = z;        /* (z*8) >> 3; */
-    int i, j, y00, y01, y10, y11, u, v;
-    unsigned char *pY = image;
-    unsigned char *pU = pY + numpix;
-    unsigned char *pV = pU + numpix / 4;
-    unsigned char *image2 = temp;
-	printf("yuv420p->rgb\n");
-    for (j = 0; j <= y - 2; j += 2) {
-        for (i = 0; i <= x - 2; i += 2) {
-            y00 = *pY;
-            y01 = *(pY + 1);
-            y10 = *(pY + x);
-            y11 = *(pY + x + 1);
-            u = (*pU++) - 128;
-            v = (*pV++) - 128;
+yuv420p_to_rgb (unsigned char *image, unsigned char *temp, int x, int y, int z) {
+	const int numpix = x * y;
+	const int bytes = z;        /* (z*8) >> 3; */
+	int i, j, y00, y01, y10, y11, u, v;
+	unsigned char *pY = image;
+	unsigned char *pU = pY + numpix;
+	unsigned char *pV = pU + numpix / 4;
+	unsigned char *image2 = temp;
+	
+#warning "FIXME: enable debugging here"
+/*	if(cam->debug) {
+		g_message("yuv420p->rgb\n");
+	}*/
+	
+	for (j = 0; j <= y - 2; j += 2) {
+            for (i = 0; i <= x - 2; i += 2) {
+                y00 = *pY;
+                y01 = *(pY + 1);
+                y10 = *(pY + x);
+                y11 = *(pY + x + 1);
+                u = (*pU++) - 128;
+                v = (*pV++) - 128;
 
-            move_420_block (y00, y01, y10, y11, u, v, x, image2, z * 8);
+                move_420_block (y00, y01, y10, y11, u, v, x, image2, z * 8);
 
-            pY += 2;
-            image2 += 2 * bytes;
+                pY += 2;
+                image2 += 2 * bytes;
+            }
+            pY += x;
+            image2 += x * bytes;
         }
-        pY += x;
-        image2 += x * bytes;
-    }
 }
+
 static inline void
 move_420_block (int yTL, int yTR, int yBL, int yBR, int u, int v,
                 int rowPixels, unsigned char *rgb, int bits)

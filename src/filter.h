5fc170fc71d6ea75351dc349a28eae07d0a1e7b7
Sven Herzberg <herzi@gnome-de.org>
2005-11-07 21:42:49 +0000

    starting to clean up cleaned up the file for the first significant
    2005-11-07  Sven Herzberg  <herzi@gnome-de.org>
    
            * configure.in: starting to clean up
    	* src/filter.h: cleaned up the file for the first significant
    	improvement

--- src/filter.h
+++ src/filter.h
@@ -1,13 +1,52 @@
-void yuv420p_to_rgb(unsigned char *image, unsigned char *temp, int x, int y, int z);
-void fix_colour(guchar *image, int x, int y);
-void negative(unsigned char *image, int x, int y, int z);
-void threshold(unsigned char *image, int x, int y, int threshold_value);
-void threshold_channel(unsigned char *image, int x, int y, int threshold_value);
-void mirror(unsigned char *image, int x, int y, int z);
-void wacky(unsigned char *image, int z, int x, int y);
-void smooth(unsigned char *image, int z, int x, int y);
+/* this file is part of camorama, a gnome webcam viewer
+ *
+ * AUTHORS
+ *	Greg Jones	     <greg@fixedgear.org>
+ *	Bastien Nocera	     <hadess@hadess.net>
+ *      Sven Herzberg        <herzi@gnome-de.org>
+ *
+ * Copyright (C) 2003 Greg Jones
+ * Copyright (C) 2003 Bastien Nocera
+ * Copyright (C) 2005 Sven Herzberg
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA
+ */
 
-void bw(unsigned char *image, int x, int y);
-void bw2(unsigned char *image, int x, int y);
-void laplace(unsigned char *image, int z, int x, int y);
-void sobel(unsigned char *image, int x, int y);
+#ifndef CAMORAMA_FILTER_H
+#define CAMORAMA_FILTER_H
+
+#include <glib/gtypes.h>
+
+G_BEGIN_DECLS
+
+void yuv420p_to_rgb	(guchar *image, guchar *temp, gint x, gint y, gint z);
+void fix_colour		(guchar *image, gint x, gint y);
+void negative		(guchar *image, gint x, gint y, gint z);
+void threshold		(guchar *image, gint x, gint y, gint threshold_value);
+void threshold_channel	(guchar *image, gint x, gint y, gint threshold_value);
+void mirror		(guchar *image, gint x, gint y, gint z);
+void wacky		(guchar *image, gint z, gint x, gint y);
+void smooth		(guchar *image, gint z, gint x, gint y);
+
+void bw			(guchar *image, gint x, gint y);
+void bw2		(guchar *image, gint x, gint y);
+void laplace		(guchar *image, gint z, gint x, gint y);
+void sobel		(guchar *image, gint x, gint y);
+
+G_END_DECLS
+
+#endif /* !CAMORAMA_FILTER_H */
+

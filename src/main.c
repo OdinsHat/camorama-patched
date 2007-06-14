9116aab5634b035677b3c98e485a9bae0adec2fd
Sven Herzberg <herzi@gnome-de.org>
2007-06-14 14:07:52 +0000

    replaced GNOMELOCALEDIR by PACKAGE_LOCALE_DIR
    2007-06-14  Sven Herzberg  <herzi@gnome-de.org>
    
    	* src/main.c: replaced GNOMELOCALEDIR by PACKAGE_LOCALE_DIR
    
    svn path=/trunk/; revision=301

--- src/main.c
+++ src/main.c
@@ -79,7 +79,7 @@ main(int argc, char *argv[]) {
     cam->video_dev = NULL;
     cam->read = FALSE;
 
-    bindtextdomain (GETTEXT_PACKAGE, GNOMELOCALEDIR);
+    bindtextdomain (GETTEXT_PACKAGE, PACKAGE_LOCALE_DIR);
     bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
     textdomain (GETTEXT_PACKAGE);
     setlocale (LC_ALL, "");

25d29b03b6dce170227460604ff58742330b5973
Sven Herzberg <sven@imendio.com>
2008-05-28 16:16:02 +0000

    Some code improvements
    2008-05-28  Sven Herzberg  <sven@imendio.com>
    
    	Some code improvements
    
    	* src/v4l.h: added G_BEGIN_DECLS and G_END_DECLS markers; added a note
    	for an upcoming field move; changed the order of the functions to be
    	more intuitive
    
    svn path=/trunk/; revision=370

--- src/v4l.h
+++ src/v4l.h
@@ -19,10 +19,16 @@
 #include <glade/glade.h>
 #include <gconf/gconf-client.h>
 
+G_BEGIN_DECLS
+
 typedef struct camera cam;
+
+G_END_DECLS
 
 #include "camorama-filter-chain.h"
 #include "capture-strategy.h"
+
+G_BEGIN_DECLS
 
 typedef enum {
 	PICMAX = 0,
@@ -49,8 +55,8 @@ struct camera {
     struct video_capability vid_cap;
     struct video_picture vid_pic;
     struct video_window vid_win;
-    struct video_mbuf vid_buf;
-    struct video_mmap vid_map;
+    struct video_mbuf vid_buf; // FIXME: move into CaptureStrategyMmapPrivate
+    struct video_mmap vid_map; // FIXME: could be moved into CaptureStrategyMmapPrivate
     char *video_dev;
     unsigned char *image;
     gchar *capturefile, *rcapturefile;
@@ -76,10 +82,12 @@ struct camera {
 };
 
 void camera_cap (cam *);
+void get_win_info (cam *);
 void set_win_info (cam * cam);
 void get_pic_info (cam *);
 void set_pic_info (cam *);
-void get_win_info (cam *);
+
+G_END_DECLS
 
 #endif /* !CAMORAMA_V4L_H */
 
